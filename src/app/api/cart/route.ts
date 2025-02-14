1232dad92c565b18e7cff0c43e54e43bcef207b1
eduardokozerski <158783271+eduardokozerski@users.noreply.github.com>
2025-02-14 19:03:10 -0400

    1st try - fixing deploy error

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export async function POST(req: Request) {
   try {
