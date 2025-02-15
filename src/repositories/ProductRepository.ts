bc2cda00204cad1d0daf72c1e58553f2b9f27ba3
eduardokozerski <158783271+eduardokozerski@users.noreply.github.com>
2025-02-14 20:19:24 -0400

    add NEXT_PUBLIC_API_URL to ProductRepository.ts

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -15,7 +15,8 @@ export type ProductResponse = {
 export class ProductRepository {
   static async fetchProducts(page: number = 1, perPage: number = 6) {
     try {
-      const response = await fetch(`http://localhost:3000/api/products?page=${page}&perPage=${perPage}`);
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      const response = await fetch(`${apiUrl}/api/products?page=${page}&perPage=${perPage}`);
 
       if (!response.ok) {
         const errorDetails = await response.text();
@@ -25,7 +26,6 @@ export class ProductRepository {
       const data: ProductResponse = await response.json();
       return data;
     } catch (error: unknown) {
-
       if (error instanceof Error) {
         console.error(error);
         throw new Error(`Erro ao buscar produtos: ${error.message}`);
